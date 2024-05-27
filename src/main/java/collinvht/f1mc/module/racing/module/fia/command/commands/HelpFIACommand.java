0acc2057e5576572db14f208a3054e0809b66fe9
Collinvht <collin060204@gmail.com>
2024-05-27 10:23:31 +0200

    Helpfia

---
+++ src/main/java/collinvht/f1mc/module/racing/module/fia/command/commands/HelpFIACommand.java
@@ -0,0 +1,30 @@
+package collinvht.f1mc.module.racing.module.fia.command.commands;
+
+import collinvht.f1mc.util.Permissions;
+import collinvht.f1mc.util.commands.CommandUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class HelpFIACommand extends CommandUtil {
+    @Override
+    protected void initializeCommand(@NotNull CommandSender commandSender) {
+        addPart("%", 1, "/helpfia [message]", ((sender, command, label, args) -> {
+            if(sender instanceof Player player) {
+                StringBuilder builder = new StringBuilder();
+                for (String arg : args) {
+                    builder.append(arg).append(" ");
+                }
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    if(Permissions.FIA_ADMIN.hasPermission(onlinePlayer)) {
+                        onlinePlayer.sendMessage(ChatColor.RED + "HELPFIA " + ChatColor.GRAY + " | " + builder);
+                    }
+                }
+                return prefix + "Message sent";
+            }
+            return "";
+        }));
+    }
+}
